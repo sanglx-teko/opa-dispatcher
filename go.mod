c196200e24195e80ff4c7d33f4cdac0b7055891a
Lý Xuân Sang <sang.lx@teko.vn>
2019-07-04 09:56:09 +0700

    create decision handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sanglx-teko/opa-dispatcher
 
 go 1.12
+
+require github.com/labstack/echo/v4 v4.1.6
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/sanglx-teko/opa-dispatcher/controller"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.POST("/decision/handler", controller.HandleDecisionAPIController)
+	e.Logger.Fatal(e.Start(":1323"))
 }
